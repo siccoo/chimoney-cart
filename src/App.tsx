3a7db8366730f32656dfb04f40f7d025df5dfaf8
Michael Chilaka <obichilaka@gmail.com>
2023-01-11 17:10:22 +0100

    necessary importation of material-ui elements & App.styles.ts barebones

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+
+// Components
+import Drawer from "@material-ui/core/Drawer";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+// Styles
+import { Wrapper } from "./App.styles";
+
 const App = () => {
   return (
     <div className="App">
