e527293b5d56ca2955c2b7e47838df4b2beb4de2
Michael Chilaka <obichilaka@gmail.com>
2023-01-13 20:18:52 +0100

    deletion & restructuring & formatCurrency.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,11 @@
-import { useState } from "react";
+import React from 'react'
 
-import Navbar from "./components/Header/Navbar";
-import Item from "./components/Item/Item";
-import Cart from "./components/Cart/Cart";
+type Props = {}
 
-import Drawer from "@material-ui/core/Drawer";
-import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
-import Badge from "@material-ui/core/Badge";
-import Grid from "@material-ui/core/Grid";
-import LinearProgress from '@material-ui/core/LinearProgress';
-
-import { Wrapper, StyledButton } from "./App.styles";
-import { useQuery } from "react-query";
-
-export type CartItemType = {
-  id: number,
-  category: string,
-  description: string,
-  image: string,
-  price: number,
-  title: string,
-  amount: number,
-};
-
-
-const getProducts = async (): Promise<CartItemType[]> => {
-  return await (await fetch("https://fakestoreapi.com/products")).json();
-};
-
-const App = () => {
-  const { data, isLoading, error } = useQuery<CartItemType[]>(
-    "products",
-    getProducts
-  )
-
-  const [cartOpen, setCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([] as CartItemType[]);
-
-  const getTotalItems = (items: CartItemType[]) => items.reduce((accumulator: number, item: CartItemType) => accumulator + item.amount, 0);
-
-  const handleAddToCart = (clickedItem: CartItemType) => {
-    setCartItems(prev => {
-      const isItemInCart = prev.find(item => item.id === clickedItem.id)
-
-      if (isItemInCart) {
-        return prev.map(item => (
-          item.id === clickedItem.id ? { ...item, amount: item.amount + 1 } : item
-        ))
-      }
-
-      return [...prev, { ...clickedItem, amount: 1 }];
-    })
-  };;
-
-  const handleRemoveFromCart = () => null;
-
-  if (isLoading) return <LinearProgress />
-  if (error) return <div>Something went wrong...</div>
-
+const App = (props: Props) => {
   return (
-    <div className="App">
-      <Wrapper>
-        <Navbar />
-        <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-          <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart} />
-        </Drawer>
-        <StyledButton onClick={() => setCartOpen(true)}>
-          <Badge badgeContent={getTotalItems(cartItems)} color="error">
-            <AddShoppingCartIcon />
-          </Badge>
-        </StyledButton>
-        <Grid container spacing={4}>
-          {data?.map(item => (
-            <Grid item key={item.id} xs={12} sm={3}>
-              <Item item={item} handleAddToCart={handleAddToCart} />
-            </Grid>
-          ))}
-        </Grid>
-      </Wrapper>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
 export default App;
